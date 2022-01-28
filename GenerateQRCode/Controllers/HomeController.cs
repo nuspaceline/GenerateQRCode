62063eba4750873aba3caf7c3596337e696b2a85
nuintuition <nuintuition@gmail.com>
2022-01-28 10:16:59 +0800

    Init Project

---
+++ GenerateQRCode/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using GenerateQRCode.Models;
+using Microsoft.AspNetCore.Mvc;
+using QRCoder;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Text;
+
+namespace GenerateQRCode.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult GetQRCodePNG(string text)
+        {
+            var qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(text, QRCodeGenerator.ECCLevel.H);
+            var qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+            using var stream = new MemoryStream();
+            qrCodeImage.Save(stream, ImageFormat.Png);
+            return File(stream.ToArray(), "image/jpeg");
+        }
+
+        public IActionResult GetQRCodeSVG(string text)
+        {
+            var qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(text, QRCodeGenerator.ECCLevel.H);
+            var svgQRCode = new SvgQRCode(qrCodeData);
+            var qrCodeImage = svgQRCode.GetGraphic(20);
+            byte[] byteArray = Encoding.UTF8.GetBytes(qrCodeImage);
+            return File(byteArray, "image/svg+xml");
+        }
+    }
+}
